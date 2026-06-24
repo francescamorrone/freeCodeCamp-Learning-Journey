ff2c043873dd8b4222f95e9239360b911aa51485
Francesca Morrone <francescamorr@hotmail.com>
2026-06-24 22:10:51 +0200

    added javascript exercises

---
+++ javascript-exercises/bot-facts.js
@@ -0,0 +1,25 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+let botName = "David Bowie";
+let botLocation = "Mars";
+let favoriteLanguage = "music";
+
+console.log("My name is " + botName +  " and I live on " + botLocation + ".")
+
+console.log("My favorite programming language is " + favoriteLanguage + ".")
+
+favoriteLanguage = "Music";
+
+let codingFact = favoriteLanguage + " is the best way to communicate."
+
+console.log(codingFact)
+
+codingFact = favoriteLanguage + " is also very fun."
+
+console.log(codingFact)
+
+codingFact = favoriteLanguage + " can make you dance or break your heart."
+
+console.log(codingFact)
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
---
+++ javascript-exercises/sentence-maker.js
@@ -0,0 +1,20 @@
+let adjective = "cowardly";
+let noun = "dog";
+let verb = "running away from danger";
+let place = "home in Nowhere";
+let adjective2 = "huge";
+let noun2 = "biscuits";
+let firstStory = "Once upon a time, there was a " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "bad";
+noun = "bunny";
+verb = "angry";
+place = "rabbit hole in Gragnano (NA)";
+adjective2 = "cute little";
+noun2 = "shoes";
+
+let secondStory = "Once upon a time, there was a " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
